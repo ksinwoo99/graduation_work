292276f6fe3c348e3c1fc236ab20df3b3f8b5041
lmaJine <lsh_5222@naver.com>
2026-05-17 22:22:02 +0900

    로그 업데이트

--- server/start.sh
+++ server/start.sh
@@ -10,7 +10,6 @@ nohup uvicorn 8000:app --host 0.0.0.0 --port 8000 --workers 4 >> log_8000.out 2>
 nohup uvicorn 8002:app --host 0.0.0.0 --port 8002 --workers 1 >> log_8002.out 2>&1 &
 
 # 2. ML Servers
-cd server
 nohup uvicorn main:app --host 0.0.0.0 --port 8001 --workers 1 >> log_8001.out 2>&1 &
 nohup python -u ml_worker.py >> ml_worker.log 2>&1 &
 
