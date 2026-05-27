6766000118ef51da1ac53265e530ffddfd2c578f
lmaJine <lsh_5222@naver.com>
2026-05-27 21:05:37 +0900

    튜토리얼 76 > 77

--- graduation_works/Assets/Scripts/Ingame_System_Save.cs
+++ graduation_works/Assets/Scripts/Ingame_System_Save.cs
@@ -171,7 +171,7 @@ public class Ingame_System_Save : MonoBehaviour {
             if (Ingame_UI_Tutorial.Instance.skipPanel != null && Ingame_UI_Tutorial.Instance.skipPanel.activeSelf) {
                 data.tutorial_step = 0;
             } else {
-                data.tutorial_step = Ingame_UI_Tutorial.Instance.isTutorialActive ? Ingame_UI_Tutorial.Instance.currentStep : 76;
+                data.tutorial_step = Ingame_UI_Tutorial.Instance.isTutorialActive ? Ingame_UI_Tutorial.Instance.currentStep : 77;
             }
         }
 
@@ -347,8 +347,8 @@ public class Ingame_System_Save : MonoBehaviour {
         if (!Shared_Manager_Session.IsVisiting && Ingame_UI_Tutorial.Instance != null && data.resources != null) {
             int savedStep = data.resources.tutorial_step;
             
-            if (savedStep == -1 || savedStep == 76 || (savedStep == 0 && data.machines != null && data.machines.Count > 0)) {
-                Ingame_UI_Tutorial.Instance.currentStep = 76;
+            if (savedStep == -1 || savedStep == 77 || (savedStep == 0 && data.machines != null && data.machines.Count > 0)) {
+                Ingame_UI_Tutorial.Instance.currentStep = 77;
                 Ingame_UI_Tutorial.Instance.EndTutorial();
                 
                 if (Ingame_UI_Help.Instance != null) {
